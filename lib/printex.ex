1c41980e2bf82bb26808d6559555cddf974fb55f
sinless-leader <sinlessleader7@gmail.com>
2020-02-07 18:24:52 +0530

    Default color updated to green.

--- lib/printex.ex
+++ lib/printex.ex
@@ -7,30 +7,30 @@ defmodule Printex do
   Printex helps you to print the data and strings in color text and color backgrounds. It make use of the `IO.ANSI` Module.
 
   ## Why Printex ?
-  This module prints the different console outputs with different colors and background colors as well.        
+  This module prints the different console outputs with different colors and background colors as well.
 
   ## Idea
-  We can see a lot of lines with regular text in your console at the time of development. It will be hard to identify any of your `IO.inspect` statements output in your console. I thought, it would be nice to highlight our `inspect` statements outputs to distinguish from the regular lines already present in the console.       
+  We can see a lot of lines with regular text in your console at the time of development. It will be hard to identify any of your `IO.inspect` statements output in your console. I thought, it would be nice to highlight our `inspect` statements outputs to distinguish from the regular lines already present in the console.
 
-  It gives you the direct focus on the console by highlighting text with colors and backgrounds.           
+  It gives you the direct focus on the console by highlighting text with colors and backgrounds.
 
   You can use this in multiple ways like printing the `:error` message and many formats as well.
-  This helps you to identify the message from the regular out put of the console. 
-  You can also specify the `:bg_color` - Background color for the text to lay on.              
+  This helps you to identify the message from the regular out put of the console.
+  You can also specify the `:bg_color` - Background color for the text to lay on.
 
-  For more examples and screen shots and how to usage check the [github](https://github.com/blackode/printex)              
+  For more examples and screen shots and how to usage check the [github](https://github.com/blackode/printex)
 
   ### NOTE
-  > Colors may vary from terminal to terminal...       
+  > Colors may vary from terminal to terminal...
 
   """
   @doc """
-  This function puts the `string` accordingly to the given params. The first argument should be the `binary`.    
-  The second parameter is the foreground color that you what the string to be print, and the third parameter is the        
-  background color used for printing the `string`. However, passing colors are optional.         
+  This function puts the `string` accordingly to the given params. The first argument should be the `binary`.
+  The second parameter is the foreground color that you what the string to be print, and the third parameter is the
+  background color used for printing the `string`. However, passing colors are optional.
 
   The colors are to be of type `atom` and at present these `[:black,:blue,:cyan,:green,:magenta,:red,:white,:yellow]`  are supported.
-  However, it is optional.    
+  However, it is optional.
 
   If the no parameter is passed then default format for printing i.e regular format `IO.puts` is used.
 
@@ -50,13 +50,13 @@ defmodule Printex do
       This prints the message in  red color
 
       iex> Printex.prints ("message", :red,:white)
-      This prints the message in red color with white background          
+      This prints the message in red color with white background
 
 
-  ### Text with foreground color                   
-  ![Prints Image](assets/images/prints.png)             
+  ### Text with foreground color
+  ![Prints Image](assets/images/prints.png)
 
-  ### Text with background colors                 
+  ### Text with background colors
   ![Prints Image](assets/images/prints_bg.png)
 
   """
@@ -68,7 +68,7 @@ defmodule Printex do
     _prints(string, color)
   end
 
-  def prints(string, color \\ :white) do
+  def prints(string, color \\ :green) do
     _prints(string, color)
   end
 
@@ -92,14 +92,14 @@ defmodule Printex do
 
   @doc """
 
-  Prints the given message in the error format. It uses `:red` as a foreground text color.        
+  Prints the given message in the error format. It uses `:red` as a foreground text color.
 
   ## Examples
 
       iex> Printex.print_error "This is error"
 
   ### Printing Error Text
-  ![Prints Image](assets/images/print_error.png)          
+  ![Prints Image](assets/images/print_error.png)
   """
 
   def print_error(string) when is_binary(string) do
@@ -112,7 +112,7 @@ defmodule Printex do
   #################
 
   @doc """
-  Prints the given message in the information format in the green color.           
+  Prints the given message in the information format in the green color.
 
 
   ## Examples
@@ -120,7 +120,7 @@ defmodule Printex do
       iex> Printex.print_info "This is info"
 
   ### Printing Information Text
-  ![Prints Image](assets/images/print_info.png)          
+  ![Prints Image](assets/images/print_info.png)
   """
 
   def print_info(string) when is_binary(string) do
@@ -133,14 +133,14 @@ defmodule Printex do
   #################
 
   @doc """
-  Prints the given message in the warning format in the yellow color.       
+  Prints the given message in the warning format in the yellow color.
 
   ## Examples
 
-      iex> Printex.print_warning "This is warning"            
+      iex> Printex.print_warning "This is warning"
 
   ### Printing Warning Text
-  ![Prints Image](assets/images/print_warning.png)          
+  ![Prints Image](assets/images/print_warning.png)
   """
   def print_warning(string) when is_binary(string) do
     ansi_format(:yellow, string, "WARNING", "warn_message")
@@ -148,7 +148,7 @@ defmodule Printex do
   end
 
   @doc """
-  This prints the given text in the format you specified. The string formats are 
+  This prints the given text in the format you specified. The string formats are
   as follows
   :x_on_y here {x,y} can be any 8 possible colors
 
@@ -159,17 +159,17 @@ defmodule Printex do
   5. magenta
   6. red
   7. white
-  8. yellow     
+  8. yellow
 
   In addition you can also specify respective light combination of colors
 
-  ## Usage 
-  `:black_on_white`    
-  prints the text in black color with white background.            
-  `:red_on_light_green`          
-  prints the text in red color with light_green background.           
-  `:light_blue_on_light_cyan`             
-  prints the text in light_blue color with light_cyan background.          
+  ## Usage
+  `:black_on_white`
+  prints the text in black color with white background.
+  `:red_on_light_green`
+  prints the text in red color with light_green background.
+  `:light_blue_on_light_cyan`
+  prints the text in light_blue color with light_cyan background.
 
   ### x_on_y format colors
   ![x_on_y image](assets/images/x_on_y.png)
@@ -236,7 +236,7 @@ defmodule Printex do
   end
 
   ####################################
-  #         Private Definitions      # 
+  #         Private Definitions      #
   ####################################
 
   defp parse_colors(atom) when is_atom(atom) do
@@ -255,7 +255,7 @@ defmodule Printex do
         color,
         """
         <<<<<<<<<<<<<<<<
-            #{type}   
+            #{type}
         >>>>>>>>>>>>>>>>
 
         -------------------------
